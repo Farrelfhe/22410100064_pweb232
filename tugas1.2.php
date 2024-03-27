7df05d45756a361e57638ef69f59ec339e6bf4e6
Farrelfhe <164366513+Farrelfhe@users.noreply.github.com>
2024-03-27 18:01:41 +0700

    Update tugas1.2.php

--- tugas1.2.php
+++ tugas1.2.php
@@ -2,6 +2,8 @@
      <head>
         <title>Tugas1.2</title>
      </head>
+     <link rel="stylesheet" href = "Mycss.css"> 
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
      <body>
         <form action="/action_page.php">
             <label for="kode">Kode:</label><br>
@@ -15,9 +17,9 @@
             <label for="Jual">Jual:</label><br>
             <input type="text" id="Jual" name="Jual" value=""><br><br>
  
-            <a href="tugas1.php" target="_self"><input type="button" value="Save"></a>
-            <a href="tugas1.php" target="_self"><input type="button" value="Delete"></a>
-            <a href="tugas1.php" target="_self"><input type="button" value="Cancel"></a>
+            <a href="tugas1.php" target="_self" class="btn"><i class="fa fa-file"></i> Save</a>&emsp;
+        <a href="tugas1.php" target="_self" class="btn"><i class="fa fa-close"></i> Cancel</a>&emsp;
+        <a href="tugas1.php" target="_self" class="btn"><i class="fa fa-trash"></i> Delete</a>
         </form> 
     
         </body>
